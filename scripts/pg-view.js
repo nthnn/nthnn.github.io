a7008cdb56e62c18b08775f1d5890485a6060d1f
nthnn <nathanneisip@gmail.com>
2023-03-21 15:57:17 +0800

    Added template.html and some projects.

--- scripts/pg-view.js
+++ scripts/pg-view.js
@@ -1,15 +1,20 @@
+let isValidID = (str)=> !isNaN(str) && !isNaN(parseFloat(str));
+
 ((paramName, callback)=> {
     var $ = new URL(window.location.href).searchParams.get(paramName);
     if($)
         callback($);
 })("id", (id)=> {
-    $.getJSON("./assets/database/projects/" + id + ".json", (data)=> {
-        document.title = data["title"];
-        $("#main-content").html(data["content"]);
-    }).fail(()=> {});
-});
+    if(isValidID(id)) {
+        $.getJSON("./assets/database/projects/" + id + ".json", (data)=> {
+            document.title = data["title"];
+            $("#main-content").html(data["content"]);
 
-baguetteBox.run(".main-section", {
-    animation: 'fadeIn',
-    noScrollBars: true,
+            baguetteBox.run(".main-section", {
+                animation: 'fadeIn',
+                noScrollBars: true,
+            });
+        }).fail(()=> {});
+    }
+    else window.location.href = "404.html";
 });
